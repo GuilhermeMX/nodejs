3b406bfadb4790d412c405b1c4b1071963589f33
Gui <guilhermenen15@gmail.com>
2022-11-01 03:55:54 -0300

    finished cpf existent

--- projects/finappi/src/index.js
+++ projects/finappi/src/index.js
@@ -1,56 +1,37 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/courses", (request, response) => {
-  const query = request.query;
-  console.log(query)
-  return response.json([
-    "Curso 1",
-    "Curso 2",
-    "Curso 3"
-  ]);
-});
+const customers = [];
 
-app.post("/courses", (request, response) => {
-  const body = request.body;
-  console.log(body)
-  return response.json([
-    "Curso 1",
-    "Curso 2",
-    "Curso 3",
-    "Curso 4"
-  ]);
-});
+/** cpf = string
+    name = string
+    id = uuid
+    statement = []
+*/
 
-app.put("/courses/:id", (request, response) => {
-  const { id } = request.params;
-  console.log(id);
-  return response.json([
-    "Curso 6",
-    "Curso 2",
-    "Curso 3",
-    "Curso 4"
-  ]);
-})
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
 
-app.patch("/courses/:id", (request, response) => {
-  return response.json([
-    "Curso 6",
-    "Curso 7",
-    "Curso 3",
-    "Curso 4"
-  ]);
-})
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf); 
 
-app.delete("/courses/:id", (request, response) => {
-  return response.json([
-    "Curso 6",
-    "Curso 7",
-    "Curso 4"
-  ]);
+  //Returning status if cpf value are equal:   
+  if (customerAlreadyExists) {
+    return response.status(400).json({error: "Customer already exists!"});
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return response.status(201).send();
 })
 
 app.listen(3333);
