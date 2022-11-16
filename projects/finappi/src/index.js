dfcf2668935edf5bd1cae6649f188c7bf20886d4
Gui <guilhermenen15@gmail.com>
2022-11-16 00:07:04 -0300

    finished withdraw system. working well.

--- projects/finappi/src/index.js
+++ projects/finappi/src/index.js
@@ -22,6 +22,20 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } 
+    else {
+      return acc - operation.amount;
+    }
+         
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -61,6 +75,29 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
     created_at: new Date(),
     type: "credit",
   }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+})
+
+//creating widthdraw:
+app.post("/widthdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({error: "insufficient funds"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
 
   customer.statement.push(statementOperation);
 
