e737186fa602ae154f7e0f6edc056ff78f7fd2b6
Gui <guilhermenen15@gmail.com>
2022-11-25 12:01:19 -0300

    create post user

--- projects/projeto-1/index.js
+++ projects/projeto-1/index.js
@@ -10,18 +10,39 @@ app.use(express.json());
 
 const users = [];
 
+// middleware:
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
 
-  const user = users.find(user => user.cpf === cpf)
+  const user = users.find(user => user.username === username)
 
   if (!user) {
     return response.status(400).json({ error: "user not found" });
   }
+
+  request.username = username;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { username, name } = request.body;
+
+  const userAlreadyExists = users.some(
+    (user) => user.username === username
+  );
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: "user already exists" })
+  };
+
+  userAlreadyExists.push({
+    id: 'uuid',
+    title: 'Nome da tarefa',
+    done: false, 
+    deadline: '2021-02-27T00:00:00.000Z', 
+    created_at: '2021-02-22T00:00:00.000Z'
+  })
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
